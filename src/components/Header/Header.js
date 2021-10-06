32adb248d2d9d52143433f29c812cbb1eab0bd11
naimurnemu <naimurrahaman82@gmail.com>
2021-10-06 10:20:50 +0600

    cart problem solve

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../../images/logo.png';
 import './Header.css';
 
@@ -7,9 +8,9 @@ const Header = () => {
         <div className="header">
             <img className="logo" src={logo} alt="" />
             <nav>
-                <a href="/shop">Shop</a>
-                <a href="/orders">Order Review</a>
-                <a href="/inventory">Manage Inventory</a>
+                <NavLink className="nav-link" to="/shop">Shop</NavLink>
+                <NavLink className="nav-link" to="/review">Order Review</NavLink>
+                <NavLink className="nav-link" to="/inventory">Manage Inventory</NavLink>
             </nav>
         </div>
     );
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProducts";
+import Cart from "../Cart/Cart";
 
 const OrderReview = () => {
+    const [products] = useProducts();
+    const [cart] = useCart(products);
+ 
     return (
         <div>
+            <h1>{products.length}</h1>
             <h2>This is Order Review</h2>
+            <h3>{cart.length}</h3>
+            <Cart cart={cart}></Cart>
         </div>
     );
 };
